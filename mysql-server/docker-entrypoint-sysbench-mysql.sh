47dbfe40954eb2511b399430e08331814d521cb2
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-10-31 01:23:17 -0700

    Changes to sysbench Dockerfile

--- mysql-server/docker-entrypoint-sysbench-mysql.sh
+++ mysql-server/docker-entrypoint-sysbench-mysql.sh
@@ -2,4 +2,4 @@
 
 sudo /etc/init.d/ssh start
 cd /benchmarktool
-python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql 
+python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql $1
