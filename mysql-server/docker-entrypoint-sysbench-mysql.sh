649d1d12b055f64bb74ea04e56b9a90a708a153e
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-10-31 01:54:59 -0700

    Changing path for default mysqldir and mysqlddir

--- mysql-server/docker-entrypoint-sysbench-mysql.sh
+++ mysql-server/docker-entrypoint-sysbench-mysql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+ls -la /
+ls -la /home/rgadgil/
 sudo /etc/init.d/ssh start
 cd /benchmarktool
-python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql $1
+python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client
