6d18ba239617b0e177544b4e6e466a3628268115
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-10-31 03:05:18 -0700

    Changes in sysbench

--- mysql-server/docker-entrypoint-sysbench-mysql.sh
+++ mysql-server/docker-entrypoint-sysbench-mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ls -la /
-ls -la /home/rgadgil/
+echo ${1}
 sudo /etc/init.d/ssh start
 cd /benchmarktool
-python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client
+python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client $1
