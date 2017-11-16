861f0dc2ff1e70d95b24cea7615112b4a1705207
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-11-16 00:29:06 -0800

    Added specint lmbench

---
+++ specint/docker-entrypoint-specint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo ${1}
+sudo /etc/init.d/ssh start
+cd /benchmarktool
+#python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client $1
