3056dc7178f3a445935731b0782a49f5e283895e
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-11-16 23:13:34 -0800

    Changed the use of 7z to use xorriso

--- specint/docker-entrypoint-specint.sh
+++ specint/docker-entrypoint-specint.sh
@@ -2,4 +2,4 @@
 echo ${1}
 sudo /etc/init.d/ssh start
 cd /benchmarktool
-#python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client $1
+python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C specint2017-docker specint $1
