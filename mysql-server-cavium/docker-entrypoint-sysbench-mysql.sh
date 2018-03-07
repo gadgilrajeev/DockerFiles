1b94417200ffd2c3fce6daa29d99675d65bcec5d
gadgilrajeev <rajeev.gadgil@cavium.com>
2018-03-07 03:53:58 -0800

    Added ganglia monitor and changed the build flags

--- mysql-server-cavium/docker-entrypoint-sysbench-mysql.sh
+++ mysql-server-cavium/docker-entrypoint-sysbench-mysql.sh
@@ -2,4 +2,4 @@
 echo ${1}
 sudo /etc/init.d/ssh start
 cd /benchmarktool
-python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/mysql-server/sql --mysqldir /home/rgadgil/mysql-server/client $1
+python3 runme.py -w x86-ivy-cb1 -u rgadgil -s 127.0.0.1 -C sysbench-docker sysbench-mysql --mysqlddir /home/rgadgil/cavium-mysql/sql --mysqldir /home/rgadgil/cavium-mysql/client $1
---
+++ mysql-server-cavium/install-gmetric-cron.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+* * * * * /usr/local/bin/gmetric-mysql.sh >> /home/rgadgil/cron.log 2>&1
+
